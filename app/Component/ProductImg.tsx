91b0eceebb19747a9ea28001d6c80596c78568d2
Lekxy-dev <pablotech2007@gmail.com>
2024-10-21 11:29:22 -0700

    Initial commit

--- app/Component/ProductImg.tsx
+++ app/Component/ProductImg.tsx
@@ -14,13 +14,13 @@ const ProductImg: React.FC<ProductImageProps> = ({
     product,
     handleColorSelect,
 }) => {
-    return <div className="grid grid-cols gap-2 h-full max-h-[500px] min-h-[300px] sm:min-h-[400px]">
-        <div className="flex flex-cols items-center justify-center gap-4 cursor-pointer border h-full max-h-[500px] min-h-[300px] sm:min-h-[400px]">
+    return <div className="grid grid-cols-6 gap-2 h-full max-h-[500px] min-h-[300px] sm:min-h-[400px]">
+        <div className="flex flex-col items-center justify-center gap-4 cursor-pointer border h-full max-h-[500px] min-h-[300px] sm:min-h-[400px]">
         {product.images.map((image: SelectedImgType) => (
             <div 
               key={image.color} 
               onClick={() => handleColorSelect(image)} 
-              className={`relative w-[80%] aspect-square rounded border-teal-300 ${cartProduct.slectedImg.color === image.color ? 'border-[1.5px]' : 'border-none'}`}
+              className={`relative w-[60%] aspect-square rounded border-teal-300 ${cartProduct.slectedImg.color === image.color ? 'border-[1.5px]' : 'border-none'}`}
             >
               <Image 
                 src={image.image} 
@@ -29,8 +29,9 @@ const ProductImg: React.FC<ProductImageProps> = ({
                 className="object-contain"
               />
             </div>
-   ))}
-</div>
+         ))}
+        </div>
+        <div></div>
     </div>
 }
 
