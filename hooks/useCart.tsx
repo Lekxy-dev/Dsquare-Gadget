a0c6f5784e6b689fd13b922a98a75fe022d9c18f
Lekxy-dev <pablotech2007@gmail.com>
2024-10-23 10:48:37 -0700

    initial commit

--- hooks/useCart.tsx
+++ hooks/useCart.tsx
@@ -1,6 +1,9 @@
-import { createContext, useContext, useState } from "react";
+import { CartProductType } from "@/app/Product/ProductDetails";
+import { createContext, useCallback, useContext, useEffect, useState } from "react";
 type CartContextType = {
     cartTotalQty: number;
+    cartPs: CartProductType[];
+    handleAddProductToCart: (product: CartProductType) => void
 }
 
 export const CartContext = createContext<CartContextType | null>(null);
@@ -8,9 +11,32 @@ interface Props {
     [propName:string]:any
 }
 export const CartContextProvider = (props: Props) => {
-    const [cartTotalQty, setCartTotalQty] = useState(10)
-    const value = {
-        cartTotalQty
+    const [cartTotalQty, setCartTotalQty] = useState(10);
+    const [cartPs,setCartProducts] = useState<CartProductType[] | null>(null)
+
+    useEffect(() => {
+        const cartItems: any = localStorage.getItem('dSquareCartItem')
+       const cPs: CartProductType[] | null = JSON.parse(cartItems)
+       setCartProducts(cPs)
+    }, [])
+   const handleAddProductToCart = useCallback((product:CartProductType) => {
+      setCartProducts((prev) => {
+         let updatedCart;
+         if (prev) {
+            updatedCart = [...prev, product];
+
+         }else {
+            updatedCart = [product];
+         }
+         localStorage.setItem('dSquareCartItem',JSON.stringify(updatedCart))
+         return updatedCart;
+      })
+   }, []);
+   
+    const value = {   
+        cartTotalQty,
+        cartPs,
+        handleAddProductToCart
     }
     return <CartContext.Provider value={value} {... props}/>
 };
